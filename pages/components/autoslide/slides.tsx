b88310fb8210a8079470be7acf6f787c0a2508c1
kyoungholee <dlrudgh1303@gmail.com>
2023-09-23 15:17:26 +0900

    메인 슬라이드 구현

--- pages/components/autoslide/slides.tsx
+++ pages/components/autoslide/slides.tsx
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import style from '@/styles/slides.module.css';
 
-export default function slides() {
-  return <div>자동 슬라이드 부분입니다.</div>;
-}
+const AutoSlider = () => {
+  const images = ['/img/baby1.jpg', '/img/baby2.jpg', '/img/baby3.jpg']; // 이미지 파일 경로 목록
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  // 다음 슬라이드로 이동하는 함수
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
+  };
+
+  // 이전 슬라이드로 이동하는 함수
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? images.length - 1 : prevSlide - 1,
+    );
+  };
+
+  useEffect(() => {
+    // 일정 시간 간격으로 다음 슬라이드로 이동하는 로직
+    const intervalId = setInterval(() => {
+      nextSlide();
+    }, 3000); // 3초마다 슬라이드 변경
+
+    // 컴포넌트가 언마운트될 때 인터벌 해제
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div className={style.sliderContainer}>
+      <button onClick={prevSlide}>이전</button>
+      <Image
+        src={images[currentSlide]}
+        width={500}
+        height={300}
+        alt={`Slide ${currentSlide + 1}`}
+      />
+      <button onClick={nextSlide}>다음</button>
+    </div>
+  );
+};
+
+export default AutoSlider;
