b056f25a8d65cf935afaecd6377d628b0b623c8c
kyoungholee <dlrudgh1303@gmail.com>
2023-10-03 01:52:25 +0900

    node.js 세팅 완료

--- pages/components/header/header.tsx
+++ pages/components/header/header.tsx
@@ -13,21 +13,34 @@ export default function header() {
           height={300}
           alt="카테고리 아이콘"
         />
-        <span>카테고리</span>
+        <span className={style.category}>카테고리</span>
       </div>
 
       <Image
-          src="/img/logo.png"
-          width={700}
-          height={300}
-          alt="카테고리 아이콘"
-        />
+        className={style.logoImage}
+        src="/img/logo.png"
+        width={700}
+        height={300}
+        alt="카테고리 아이콘"
+      />
       <form>
-        <label>
-          <input placeholder="생각나는 나들이 장소를 검색해보세요." />
-        </label>
+        <div className={style.searchContainer}>
+          <input
+            placeholder="생각나는 나들이 장소를 검색해보세요."
+            className={style.search}
+          />
+        </div>
       </form>
-      <span>찜 목록</span>
+      <div className={style.heartContainer}>
+        <Image
+          className={style.heartImage}
+          src="/img/blackheart.png"
+          width={400}
+          height={300}
+          alt="좋아요 아이콘"
+        />
+        <span className={style.heart}>찜 목록</span>
+      </div>
       <span>마이 카테고리</span>
     </div>
   );
